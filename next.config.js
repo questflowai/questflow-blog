c6d3d71965ca38a90d06496cee29d85e42a4057c
GoDotDotDot <chukui0202@gmail.com>
2024-01-21 20:06:50 +0800

    feat: optm

--- next.config.js
+++ next.config.js
@@ -18,5 +18,14 @@ module.exports = withBundleAnalyzer({
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/:path*',
+        destination: 'https://app.posthog.com/:path*',
+      },
+    ];
+  },
+  compress: true,
 })
